bd9f3bf2a6c3b94c59e4194da24a2704bc29ab0c
jainprachi25 <jainprachi.55@gmail.com>
2015-10-09 14:40:07 -0700

    light api to get data in node

--- routes/light.js
+++ routes/light.js
@@ -4,14 +4,22 @@ var app = express();
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('https');
+var Hue = require('philips-hue-api');
+var username = 'newdeveloper'
 
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-router.get('/light', function(req,res){
-  
-});
-
+router.get('/getlight', function(req,res){
+    hue = Hue('http://localhost:8000/api/newdeveloper');
+    // hue = Hue('http://192.168.x.x/api/your_username');
+    hue.lights().list(function(error, lights){
+    	console.log(lights);
+    });
+  });  
 module.exports = router;
+
+
